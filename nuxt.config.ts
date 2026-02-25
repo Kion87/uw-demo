ac7079ca9cceb694803de26ee3770f05532a16c8
81864044+Kion87@users.noreply.github.com <ca.ion87@gmail.com>
2026-02-25 12:27:06 +0100

    Update1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,10 @@ export default defineNuxtConfig({
 
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+
+  nitro: {
+    externals: {
+      inline: ["@prisma/adapter-pg", "pg"],
+    },
+  },
 });
