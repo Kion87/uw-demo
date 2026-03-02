a04fbbbfa2682913e770fc9eaab179968e4aeb39
81864044+Kion87@users.noreply.github.com <ca.ion87@gmail.com>
2026-03-02 18:42:27 +0100

    new changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@netlify/nuxt"],
   css: ["~/assets/css/main.css"],
 
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
   nitro: {
+    preset: "netlify", // Functions mode
     externals: {
       inline: ["@prisma/adapter-pg", "pg"],
     },
