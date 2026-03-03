091df7d63eca9db6031543dc10db175f3c97948a
81864044+Kion87@users.noreply.github.com <ca.ion87@gmail.com>
2026-03-03 09:15:24 +0100

    Remove @netlify/nuxt from nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@netlify/nuxt"],
+  modules: ["@nuxtjs/tailwindcss"],
   css: ["~/assets/css/main.css"],
 
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
   nitro: {
-    preset: "netlify", // Functions mode
+    preset: "netlify",
     externals: {
       inline: ["@prisma/adapter-pg", "pg"],
     },
